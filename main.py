8e87903d6f122983696d45ca2a7796b16470b966
kor0p <viktorua25.03@gmail.com>
2023-11-15 09:57:27 +0200

    Підключаємо базу даних

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 from telebot import TeleBot
+import psycopg
+import os
 
 bot = TeleBot('<TOKEN>', threaded=False)
+DB_URL = os.environ['POSTGRES_URL']
+with psycopg.connect(DB_URL) as conn:
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id BIGINT PRIMARY KEY,
+        ts TIMESTAMP DEFAULT NOW()
+    )
+    """)
 
 
 @bot.message_handler()
 def main(msg):
-    bot.send_message(msg.chat.id, msg.text)
+    user_id = msg.from_user.id
+    with psycopg.connect(DB_URL) as conn:
+        conn.execute(
+            'INSERT INTO users VALUES (%s)'
+            'ON CONFLICT DO NOTHING', (user_id,),
+        )
+        sign_up_ts = conn.execute(
+            'SELECT ts FROM users WHERE id = %s',
+            (user_id,),
+        ).fetchone()[0]
+    bot.send_message(
+        user_id, 'Час твоєї реєстрації: '
+        f'{sign_up_ts:%d.%m.%Y %H:%M:%S}'
+    )
